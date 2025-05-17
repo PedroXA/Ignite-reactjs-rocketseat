ac8cb020e27bdd8d035fe14a60d032981e6991a6
PedroXA <pedroxad@gmail.com>
2025-05-17 14:59:20 -0300

    algumas configurações do babel

--- 1-GITHUB-EXPLORER/babel.config.js
+++ 1-GITHUB-EXPLORER/babel.config.js
@@ -1,6 +1,13 @@
-module.exports= {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-react'
-    ]
+const { runtime } = require("webpack");
+
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
 };
