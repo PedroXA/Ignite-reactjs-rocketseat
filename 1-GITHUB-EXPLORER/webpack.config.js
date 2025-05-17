d4a455c17849c25aa35130e2a9723d86d9d3b3db
PedroXA <pedroxad@gmail.com>
2025-05-17 18:24:11 -0300

    algumas configurações para o ambiente de desenvolvimento

--- 1-GITHUB-EXPLORER/webpack.config.js
+++ 1-GITHUB-EXPLORER/webpack.config.js
@@ -2,9 +2,14 @@
 // Para não rolar nenhum problema
 // Vai ser utilizado a lib path
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// process.ev - Localização de onde as variáveis ambiente são criadas
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: "development", // Alterando o modo entre development e production
+  mode: isDevelopment ? "development" : "production", // Alterando o modo entre development e production com condicionais
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"), // Arquivo inicial da minha aplicação
   output: {
     path: path.resolve(__dirname, "dist"), // Caminho o caminho de output
@@ -13,12 +18,28 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"], // ler os formatos js e jsx
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      inject: "body",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/, // Identificando o arquivo jsx
         exclude: /node_modules/,
         use: "babel-loader", // convertendo o arquivo para o browser
+      },
+      {
+        test: /\.scss$/, // Identificando o arquivo jsx
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"], // convertendo o arquivo para o browser
       },
     ],
   },
