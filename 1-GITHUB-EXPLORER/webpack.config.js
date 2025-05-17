a07cffca45ca2635592b903f4b5822b83e01d37e
PedroXA <pedroxad@gmail.com>
2025-05-17 14:58:35 -0300

    adicionei o path para evitar diferenças de SO na hora de colocar caminhos de diretório. Além de ter definido o mode do webpack para development e adicionado documentação para o arquivo

--- 1-GITHUB-EXPLORER/webpack.config.js
+++ 1-GITHUB-EXPLORER/webpack.config.js
@@ -1,22 +1,25 @@
 // Por algumas diferenças de SO, o meio que os caminhos são feitos é diferente.
-// Para não rolar nenhum problema 
+// Para não rolar nenhum problema
 // Vai ser utilizado a lib path
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), // Arquivo inicial da minha aplicação
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js','.jsx'],
-    },
-    module: {
-        rules: {
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            use: 'babel-loader',
-        }
-    }
-}
+  mode: "development", // Alterando o modo entre development e production
+  entry: path.resolve(__dirname, "src", "index.jsx"), // Arquivo inicial da minha aplicação
+  output: {
+    path: path.resolve(__dirname, "dist"), // Caminho o caminho de output
+    filename: "bundle.js", // Filename do arquivo de output
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // ler os formatos js e jsx
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, // Identificando o arquivo jsx
+        exclude: /node_modules/,
+        use: "babel-loader", // convertendo o arquivo para o browser
+      },
+    ],
+  },
+};
