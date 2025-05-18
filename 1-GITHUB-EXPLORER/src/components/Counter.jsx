fc3b514c124727b76abeaa907c4bd74499d948b2
PedroXA <pedroxad@gmail.com>
2025-05-18 13:40:16 -0300

    documentação para estudo

--- 1-GITHUB-EXPLORER/src/components/Counter.jsx
+++ 1-GITHUB-EXPLORER/src/components/Counter.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 // hook -> gancho
 
+// imutabilidade -> não pode alterar o conteúdo dela diretamente mas posso dar um novo valor para ela
+// const usuarios = ["pedro", "Dudu", "Damas"];
+// usuarios.push("Novo user");
+// Estou realizando uma mutação no valor de uma variável, coisa que não fazem no react
+// O correto seria
+// novoUsuario = [...usuarios, "Novo user"]
+// Sempre crio um novo espaço na memoria com a nova info do que alterar o dado que já tem na memória
+
 export function Counter() {
   const [counter, setCounter] = useState(0);
 
