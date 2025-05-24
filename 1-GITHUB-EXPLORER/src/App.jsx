84c78b961d3e6d2347f529fa4b0dc8791f176cbd
PedroXA <pedroxad@gmail.com>
2025-05-24 16:04:15 -0300

    removed: removi desmonstração antiga e sem uso de código

--- 1-GITHUB-EXPLORER/src/App.jsx
+++ 1-GITHUB-EXPLORER/src/App.jsx
@@ -2,8 +2,6 @@ import "../styles/global.scss";
 import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  // throw new Error('Deu alguma coisa de ruim aqui!')
-
   return (
     <>
       <RepositoryList />
