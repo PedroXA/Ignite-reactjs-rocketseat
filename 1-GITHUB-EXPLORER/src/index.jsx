8ba722c890c22b7b006900349ffb1f46fd080b2e
PedroXA <pedroxad@gmail.com>
2025-05-24 16:03:45 -0300

    changed: jsx para tsx

--- 1-GITHUB-EXPLORER/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './App'
-
-
-// O primeiro é o que eu quero renderizar e o segundo é onde que eu quero renderizar
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
