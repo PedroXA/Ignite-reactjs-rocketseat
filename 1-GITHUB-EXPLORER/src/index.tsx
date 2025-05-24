59e1fbf0fd291db7dfde93ce719a29f989a0e265
PedroXA <pedroxad@gmail.com>
2025-05-24 16:04:32 -0300

    changed: tipagem do index a passagem para tsx

---
+++ 1-GITHUB-EXPLORER/src/index.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+// O primeiro é o que eu quero renderizar e o segundo é onde que eu quero renderizar
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
